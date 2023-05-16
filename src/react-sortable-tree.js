171438b3aafa22ac06e6796f7c2948fb96e5d6cd
vortilla <akash.patel@vortilla.com>
2023-05-16 14:46:23 +0530

    type changed

--- src/react-sortable-tree.js
+++ src/react-sortable-tree.js
@@ -841,7 +841,7 @@ ReactSortableTree.propTypes = {
   // `subtitle` is a secondary label for the node
   // `expanded` shows children of the node if true, or hides them if false. Defaults to false.
   // `children` is an array of child nodes belonging to the node.
-  treeData: PropTypes.arrayOf(object).isRequired,
+  treeData: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 
   // Style applied to the container wrapping the tree (style defaults to {height: '100%'})
   style: PropTypes.shape({}),
